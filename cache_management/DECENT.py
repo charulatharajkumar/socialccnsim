3c41700339b1348127529ae680e5de452ff27c7b
charulatharajkumar <55196851+charulatharajkumar@users.noreply.github.com>
2019-09-25 21:46:32 +0530

    Create DECENT.py

---
+++ cache_management/DECENT.py
@@ -0,0 +1,31 @@
+#
+from cache_manager import CacheManager
+import networkx
+
+class CACHELESS(CacheManager):
+    def _init_strategy(self):
+        self.central_degree = networkx.algorithms.centrality.degree_centrality(self.topology)
+    def retrieve_from_caches(self, interest, path):
+        content_found_caches = False
+
+        for i in range(0, len(path)):
+            p = path[i]
+            if self.lookup_cache(p, interest):
+                content_found_caches = True
+                break
+            else:
+                pass
+
+        
+        #Cache in the node with the biggest central betweenness in the path
+        max_v = -1
+        node = None
+        for i in range(0, len(path)):
+            p = path[i]
+            if self.central_degree[p] > max_v:
+                max_v = self.central_degree[p]
+                node = p
+        
+        self.store_cache(node, interest)
+
+        return (content_found_caches, i)
